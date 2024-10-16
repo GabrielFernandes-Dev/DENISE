b0a2ca4854980956e5f757c1fe4957309151b2a0
Gabriela Abreu <gabrielaabreu@edu.unifor.br>
2024-10-16 15:48:24 -0300

    correcao movimento virus

--- COGITO/Scripts/demo_denise_01.gd
+++ COGITO/Scripts/demo_denise_01.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
-var area1 = Vector3(1, 0, 2)
+@export var virus1: CharacterBody3D
+#@export var enemy2: NodePath
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var pos = area1 #adicionar lógica para definir área em que cada vírus de movimenta
-	$virus.get_area_main_scene(pos)
+	randomize()
+	# Atribuir direções aleatórias aos inimigos
+	var direction1 = Vector3(randf_range(0, 0.5), 0, randf_range(-0.5,0)).normalized()
+	#gerar direções para outros vírus presentes na cena
+	#var direction2 = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
+	
+	$virus.set_direction(direction1)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
